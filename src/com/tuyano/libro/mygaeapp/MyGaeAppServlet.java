7a71a8d06888c03e9e06156f66272577c90bdef1
kokiTakahashi <kokitakahashi@192.168.100.100>
2015-06-14 23:35:45 +0900

    初回コミット

---
+++ src/com/tuyano/libro/mygaeapp/MyGaeAppServlet.java
@@ -0,0 +1,13 @@
+package com.tuyano.libro.mygaeapp;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class MyGaeAppServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
